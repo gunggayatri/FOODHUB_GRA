0d62271a7e28ab04505511f7221e471c4cfa7a29
gunggayatri <153573375+gunggayatri@users.noreply.github.com>
2025-05-15 10:47:04 +0800

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+//deklarasi variabel dimana ini adalah database kita
+$db_username = "root"; //secara default menggunakan root
+$db_hostname = "localhost"; //alamat database
+$db_password = ""; //secara default password database adalah blank atau kosong, jika database kalian sudah diberikan password silahkan ditambahkan di dalam tanda ""
+$db_name = "foodhub"; //nama database
+//--------------------------------
+$con = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+
+// Check connection
+if (mysqli_connect_errno()) {
+       echo "Koneksi database gagal : " . mysqli_connect_error();
+}else {
+ echo "Koneksi Berhasil";
+ }
+?>
